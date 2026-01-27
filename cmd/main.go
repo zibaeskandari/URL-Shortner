35ad8a18d3b857723a080825450fa81bde47441a
max-15s <amin.niazi@gmail.com>
2026-01-28 01:19:59 +0330

    feat: Add viper config loader

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"URLShortner/pkg"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("URL Shortener Project Started!")
+	config, err := pkg.LoadConfig("./configs/" + getConfigProfile() + ".yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Config: %+v\n", config)
+}
+
+func getConfigProfile() string {
+	env := os.Getenv("APP_SERVER_ENV")
+	if env == "" {
+		env = "dev"
+	}
+	return env
 }
